f3f4d616353853cf513a98de7797b086abec8637
Cicera Ribeiro <maccicera@gmail.com>
2022-08-11 15:26:08 -0300

    1059

---
+++ 6 - Questões - 11-08-2022/P_1059/P_1059.js
@@ -0,0 +1,11 @@
+
+// var input = require('fs').readFileSync('stdin','utf8');
+
+// for(let par = 0; par <=100; par++){
+//     if(par % 2 === 0)
+//     console.log(par)
+// }
+
+for (let par = 2; par <= 100; par = par + 2) {
+    console.log(par)
+}
