609a82bfe60bf072c0a5ea016e473ff411fdd091
Cicera Ribeiro <maccicera@gmail.com>
2022-08-12 13:54:53 -0300

    1075

--- 7 - Questões - 12-08-2022/P_1075/P_1075.js
+++ 7 - Questões - 12-08-2022/P_1075/P_1075.js
@@ -1,4 +1,19 @@
+// Leia um valor inteiro N.
+// Apresente todos os números entre 
+// 1 e 10000 que divididos por N dão resto igual a 2.
 
-var input = require('fs').readFileSync('stdin','utf8');
+// Entrada
+// A entrada contém um valor inteiro N(N < 10000).
 
-var valores = input.split("\n");
+//     Saída
+// Imprima todos valores que quando divididos por 
+// N dão resto = 2, um por linha.
+var input = require('fs').readFileSync('stdin', 'utf8');
+
+var n = Number(input)
+
+for (i = 1; i <= 10000; i++) {
+    if (i % n == 2) {
+        console.log(i)
+    }
+}
